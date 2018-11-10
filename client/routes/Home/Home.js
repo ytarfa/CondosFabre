de8d1948aa4200426d03a8a323f8f2576632eaa6
Yannis Tarfa <yannistarfa98@gmail.com>
2018-11-10 15:12:14 -0500

    Styled quartier diapos on home page

--- client/routes/Home/Home.js
+++ client/routes/Home/Home.js
@@ -81,8 +81,8 @@ export default class Home extends React.Component{
                 <div class="diapo" key={index}
                     style={{
                         top: 0,
-                        left: index*700 + "px",
-                        transform: 'translate('+ this.state.plateauCounter*700 + 'px)',
+                        left: index*500 + "px",
+                        transform: 'translate('+ this.state.plateauCounter*500 + 'px)',
                         transition: 'all 0.25s'
                     }}
                 >
@@ -91,6 +91,17 @@ export default class Home extends React.Component{
                         {diapo.description}
                     </div>
                 </div>
+        )
+        const quartierCarouselDescription = quartierDiapos.diapos.map((diapo, index) => 
+                    <p 
+                        key="index"
+                        style= {{
+                            opacity: this.state.plateauCounter == -index ? '1' : '0',
+                            transition: 'all 0.25s'
+                        }}
+                    >
+                        {diapo.description}
+                    </p>
         )
 
         return (
@@ -140,10 +151,26 @@ export default class Home extends React.Component{
                         <div class="diapos-container"> 
                             {quartierCarousel}
                         </div>
+                        <div class="description-container">
+                            <div class="description">
+                                {quartierCarouselDescription}
+                            </div>
+                            <div class="controls">
+                                <div class="button-container">
+                                    <button onClick={() => this.plateauCounter(1)}
+                                    style={{
+                                        opacity: this.state.plateauCounter == 0 ? '0.2' : '1'
+                                    }}
+                                    > <i class="fas fa-chevron-left fa-3x"></i> </button>
+                                    <button onClick={() => this.plateauCounter(-1)}
+                                        style={{
+                                            opacity: this.state.plateauCounter == -2 ? '0.2' : '1'
+                                        }}
+                                    > <i class="fas fa-chevron-right fa-3x"></i> </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <button onClick={() => this.plateauCounter(-1)}> - </button>
-                    <button onClick={() => this.plateauCounter(1)}> + </button>
                 </div>
 
             </div>
