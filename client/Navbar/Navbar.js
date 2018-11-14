7f4f1b258fcf6c629e8316d42ea4fae6e5aaa08a
Yannis Tarfa <yannistarfa98@gmail.com>
2018-11-14 15:13:45 -0500

    Added logo link to home page

--- client/Navbar/Navbar.js
+++ client/Navbar/Navbar.js
@@ -54,9 +54,9 @@ export default class Navbar extends React.Component{
             <div id="navbar-component">
                 <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
 
-                    <a class="navbar-brand" href="#">
+                    <Link class="navbar-brand" to="/">
                         <img src="/images/logo.png"/>
-                    </a>
+                    </Link>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon" ></span>
                     </button>
