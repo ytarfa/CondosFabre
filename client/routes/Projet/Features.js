aaeacfea01d00423c5494abada5c6cd84b24d3a5
Yannis Tarfa <yannistarfa98@gmail.com>
2018-11-18 14:28:56 -0500

    First iteration of projet page. Header, intro and feature cubes.

---
+++ client/routes/Projet/Features.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Features extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            activeFeature: 0
+        }
+        this.activateFeature = this.activateFeature.bind(this);
+    } 
+
+    activateFeature(index) {
+        this.setState({activeFeature: index});
+    }
+
+    render() {
+
+        const data = [
+            ['fas fa-home fa-fw', 'Facade'],
+            ['fas fa-solar-panel fa-fw', 'Toiture'],
+            ['fas fa-tint fa-fw', 'Robinetterie'],
+            ['fas fa-thermometer-three-quarters fa-fw', 'Température'],
+            ['fas fa-wind fa-fw', 'Échangeur d\'air'],
+            ['fas fa-volume-up fa-fw', 'Insonorisation'],
+            ['fas fa-pencil-ruler fa-fw', 'Sur mesure'],
+            ['fab fa-windows fa-fw', 'Fenêtres'],
+            ['fas fa-home fa-fw', 'Plafonds'],
+            ['fas fa-bicycle fa-fw', 'Vélo']
+        ]
+
+        const cubes = data.map((data, index) => {
+            if (index == 9) {
+                this.featureClass = this.state.activeFeature == index ? 'last feature active' : 'last feature';
+            } else if (index == 8) {
+                this.featureClass = this.state.activeFeature == index ? 'before-last feature active' : 'before-last feature';
+            } else if (index % 2 == 0) {  
+                this.featureClass = this.state.activeFeature == index ? 'first feature active' : 'first feature';
+            } else {
+                this.featureClass = this.state.activeFeature == index ? 'second feature active' : 'second feature';
+            }
+            return (
+                <span 
+                    key={index} 
+                    className={ this.featureClass } 
+                    onClick={() => this.activateFeature(index)}
+                >
+                <i class={data[0]}></i>
+                <p>{data[1]}</p>
+                </span>
+            )
+        })
+
+        const descriptions = data.map((data, index) => 
+            <p 
+                key={index}
+                style={{
+                    opacity: this.state.activeFeature == index ? '1' : '0'
+                }}
+            >
+                {data[0]}
+            </p>
+        )
+
+        return (
+            <div class="Features">
+
+                <div class="cubes">
+                    {cubes}
+                </div>
+
+                <div class="description">
+                    {descriptions}
+                </div>
+                
+            </div>
+        )
+    }
+}
--- client/routes/Projet/Projet.js
+++ client/routes/Projet/Projet.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../../scss/Projet/main.scss';
 
+import LazyImage from '../../components/LazyImage';
+import Features from './Features';
 export default class ProjetContainer extends React.Component {
 
     componentDidMount() {
@@ -9,8 +12,23 @@ export default class ProjetContainer extends React.Component {
 
     render() {
         return (
-            <div>
-                
+            <div class="Projet">
+                <div class="projet-header-container">
+                    <LazyImage hd="/images/maquette-placeholder.jpg" pre="/images/maquette-placeholder.jpg" />
+                </div>
+                <div class="maquette-promotion">
+                    <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h1>
+                    <div class="maquette-info">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Turpis massa sed elementum tempus egestas sed. Risus sed vulputate odio ut enim blandit. Neque aliquam vestibulum morbi blandit cursus risus at.</p>
+                        <ul>
+                            <li><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></li>
+                            <li><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></li>
+                            <li><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></li>
+                            <li><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></li>
+                        </ul>
+                    </div>
+                </div>
+                <Features />
             </div>
         )
     }
