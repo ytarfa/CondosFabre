1ba5666039108aac5cef1e32879d526ca5b3ab35
Yannis Tarfa <yannistarfa98@gmail.com>
2018-12-29 11:31:07 -0500

    Changed quartier view setup. Added feature carousel.

--- client/routes/Quartier/Quartier.js
+++ client/routes/Quartier/Quartier.js
@@ -5,6 +5,7 @@ import '../../scss/Quartier/main.scss';
 import Video from './Video';
 import QuartierMap from './QuartierMap';
 import LazyImage from '../../components/LazyImage';
+import Footer from '../../components/Footer';
 import CarouselComponent from './CarouselComponent';
 
 export default class QuartierContainer extends React.Component {
@@ -12,11 +13,12 @@ export default class QuartierContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeFeature: 0,
+            featureCounter: 0,
             zoom: 14,
             center: { lat:  45.517726, lng: -73.576570 }
         }
         this.panMap = this.panMap.bind(this);
+        this.counter = this.counter.bind(this);
     }
 
     componentDidMount() {
@@ -27,56 +29,94 @@ export default class QuartierContainer extends React.Component {
         this.refs.map._map.panTo(coordinates)
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if ((this.state.activeFeature == 1) && (prevState.activeFeature != 1)) {
-            this.panMap({lat:  45.526461, lng: -73.569323})
-        } else if ((this.state.activeFeature == 0) && (prevState.activeFeature != 0)) {
-            this.panMap({ lat:  45.517726, lng: -73.576570 })
-        } else if ((this.state.activeFeature == 2) && (prevState.activeFeature != 2)) {
-            this.panMap({lat:  45.519380, lng: -73.572821})
-        }
-    }
-
-    static getDerivedStateFromProps(props, state) {
-        let activeFeature = 0;
-        if(props.position <= 1000) {
-            activeFeature = 0;
-        } else if (props.position < 1600 && props.position > 1000) {
-            activeFeature = 1;
+    counter(x) {
+        let coordinateArray = [
+            {lat:  45.526461, lng: -73.569323},
+            {lat:  45.517726, lng: -73.576570},
+            {lat:  45.519380, lng: -73.572821}
+        ]
+        if (this.state.featureCounter == -2) {
+            if (x==1) {
+                this.setState((state) => {
+                    this.panMap(coordinateArray[-(state.featureCounter + x)])
+                    return { featureCounter: state.featureCounter + x}
+                });
+            }
+        } else if (this.state.featureCounter == 0) {
+            if (x==-1) {
+                this.setState((state) => {
+                    this.panMap(coordinateArray[-(state.featureCounter + x)])
+                    return { featureCounter: state.featureCounter + x}
+                })
+            }
         } else {
-            activeFeature = 2;
-        }
-        return {
-            activeFeature: activeFeature
+            this.setState((state) => {
+                this.panMap(coordinateArray[-(state.featureCounter + x)])
+                return { featureCounter: state.featureCounter + x}
+            })
         }
     }
 
     render() {
+
+        let features = ['1', '2', '3', '4'];
+
         return (
             <div class="Quartier">
-                {/* <Video /> */}
-
-                <div class="quartier-nav">
-                    <ul>
-                       <li><p>Parc Lafontaine</p></li> 
-                       <li><p>Rue Rachel</p></li> 
-                       <li><p>Boulevard Mont-Royal</p></li> 
-                       <li><p>Parc Jeanne-Mance</p></li> 
-                    </ul>
-                </div>
-
                 <div class="map-animation-container">
 
                     <div class="feature-container">
-                        <div class="feature lafontaine">
+
+                        <div class="feature lafontaine" style={{
+                            left: `${this.state.featureCounter * 100}%`,
+                            opacity: this.state.featureCounter == 0 ? 1 : 0
+                        }}>
                             <div class="title">
-                                <h1 class="background-title"> Parc Lafontaine </h1>
-                                <h1 class="foreground-title"> Parc Lafontaine </h1>
+                                <h1 class="background-title"> Le Plateau </h1>
+                                <h1 class="foreground-title"> Le Plateau </h1>
                             </div>
-                            <CarouselComponent array={[{imageScaled: 'plateau-park-scaled.jpg', image: 'plateau-park.jpg', description: 'hi'}, {imageScaled: 'plateau-park-scaled.jpg', image: 'plateau-park.jpg', description: 'hi'}]}/>
+                            {/* <Video /> */}
+                            {/* <CarouselComponent array={[{imageScaled: 'plateau-park-scaled.jpg', image: 'plateau-park.jpg', description: 'hi'}, {imageScaled: 'plateau-park-scaled.jpg', image: 'plateau-park.jpg', description: 'hi'}]}/> */}
                         </div>
-                        <div class="feature"></div>
-                        <div class="feature"></div>
+
+                        <div class="feature lafontaine" style={{
+                            left: `${(this.state.featureCounter+1) * 100}%`,
+                            opacity: this.state.featureCounter == -1 ? 1 : 0
+                        }}>
+                            <div class="title">
+                                <h1 class="background-title"> Boul. St-Laurent </h1>
+                                <h1 class="foreground-title"> Boul. St-Laurent </h1>
+                            </div>
+                        </div>
+
+                        <div class="feature lafontaine" style={{
+                            left: `${(this.state.featureCounter+2) * 100}%`,
+                            opacity: this.state.featureCounter == -2 ? 1 : 0
+                        }}>
+                            <div class="title">
+                                <h1 class="background-title"> Rue Rachel </h1>
+                                <h1 class="foreground-title"> Rue Rachel </h1>
+                            </div>
+                        </div>
+
+                        <div class="controls">
+                            <div class="button-container">
+                                <button onClick={() => this.counter(1)}
+                                    style={{
+                                        opacity: this.state.featureCounter == 0 ? '0.2' : '1'
+                                    }}
+                                > <i class="fas fa-chevron-left fa-3x"></i> </button>
+                                <button onClick={() => this.counter(-1)}
+                                    style={{
+                                        opacity: this.state.featureCounter == -2 ? '0.2' : '1'
+                                    }}
+                                > <i class="fas fa-chevron-right fa-3x"></i> </button>
+                            </div>
+                        </div>
+
+                        <div class="footer-container">
+                            <Footer />
+                        </div>
                     </div>
 
                     <div class="map-container">
@@ -87,9 +127,7 @@ export default class QuartierContainer extends React.Component {
                             ref='map'
                         />
                     </div>
-
                 </div>
-
             </div>
         )
     }
