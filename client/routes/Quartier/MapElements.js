e4bcf46e9e74aa83638bda49e19ad06813bedf3f
Yannis Tarfa <yannistarfa98@gmail.com>
2018-12-31 13:07:30 -0500

    Added spring animations to map ui elements

--- client/routes/Quartier/MapElements.js
+++ client/routes/Quartier/MapElements.js
@@ -1,41 +1,118 @@
 import React from 'react';
 
 import * as mapData from './mapData';
-
 import { Marker, Polygon, Polyline } from 'react-google-maps';
+import { Spring } from 'react-spring';
+
+class PolylineSpring extends React.Component{
+    render() {
+        return (
+            <Spring
+                from={{
+                    strokeOpacity: this.props.featureToggle ? 0 : 1, 
+                    fillOpacity: this.props.featureToggle ? 0 : 0.3
+                }} 
+                to={{
+                    strokeOpacity: this.props.featureToggle ? 1 : 0, 
+                    fillOpacity: this.props.featureToggle ? 0.3 : 0
+                }}
+            >
+                {springProps => <PolylineSpringContent path={this.props.path} {...springProps} />}
+
+            </Spring>
+        )
+    }
+}
+class PolylineSpringContent extends React.Component{
+    render() {
+
+        const styleOptions = {
+            strokeColor: '#DB4437'
+        }
+
+        return (
+            <Polyline
+                path={this.props.path}
+                options={{
+                    ...styleOptions,
+                    strokeOpacity: this.props.strokeOpacity
+                }}
+            />
+        )
+    }
+}
+
+class PolygonSpring extends React.Component{
+    render() {
+        return (
+            <Spring
+                from={{
+                    strokeOpacity: this.props.featureToggle ? 0 : 1, 
+                    fillOpacity: this.props.featureToggle ? 0 : 0.3
+                }} 
+                to={{
+                    strokeOpacity: this.props.featureToggle ? 1 : 0, 
+                    fillOpacity: this.props.featureToggle ? 0.3 : 0
+                }}
+            >
+                {springProps => <PolygonSpringContent path={this.props.path} {...springProps} />}
+
+            </Spring>
+        )
+    }
+}
+class PolygonSpringContent extends React.Component{
+    render() {
+
+        const styleOptions = {
+            strokeColor: '#DB4437',
+            fillColor: '#DB4437'
+        }
+
+        return (
+            <Polygon
+                path={this.props.path}
+                options={{
+                    ...styleOptions,
+                    fillOpacity: this.props.fillOpacity,
+                    strokeOpacity: this.props.strokeOpacity
+                }}
+            />
+        )
+    }
+}
 
 export default class MapElement extends React.Component{
     render() {
+
         return (
             <div>
                 {/* Plateau */}
-                <Polygon
+                <PolygonSpring
+                    featureToggle={this.props.feature == 'plateau'}
                     path={mapData.plateau_path}
-                    visible={this.props.feature == 'plateau'}
                 />
 
+
                 {/* Rachel */}
-                <Polyline
+                <PolylineSpring
+                    featureToggle={this.props.feature == 'rachel'}
                     path={mapData.rachel_path}
-                    visible={this.props.feature == 'rachel'}
                 />
 
                 {/* Parc Lafontaine */}
-                <Polygon
+                <PolygonSpring
+                    featureToggle={this.props.feature == 'parclafontaine'}
                     path={mapData.parclafontaine_path}
-                    visible={this.props.feature == 'parclafontaine'}
                 />
 
                 {/* Mont-Royal */}
-                <Polyline
+                <PolylineSpring
+                    featureToggle={this.props.feature == 'montroyal'}
                     path={mapData.montRoyal_path}
-                    visible={this.props.feature == 'montroyal'}
                 />
 
                 {/* St-Laurent */}
-                {/* <Polyline
-
-                /> */}
 
             </div>
         )
