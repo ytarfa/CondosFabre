20b3bcf58c9f59de7bde089057a5239a32c97497
Yannis Tarfa <yannistarfa98@gmail.com>
2019-01-18 11:31:50 -0500

    Added content and fixed layout of transport feature in quartier page.

--- client/routes/Quartier/Features/Transport.js
+++ client/routes/Quartier/Features/Transport.js
@@ -15,12 +15,26 @@ export default class Transport extends React.Component {
                     <h1 class="foreground-title"> Transport </h1>
                 </div>
 
-                <div class="stm-text-container">
+                <div class="intro-text-container">
                     <p>Quoi de mieux que de pouvoir vivre dans un quartier animé et agréable, tout en profitant de tous les accès de transports à proximité? Le Plateau Mont-Royal répond aux besoins de tout le monde, et s’adapte à vos préférences! Vous aimez vous déplacer à pieds, et marcher pour découvrir les moindres recoins de votre beau quartier? L’agencement du Plateau Mont-Royal permet aux piétons de circuler librement et facilement, pour pouvoir se rendre où ils le veulent, comme ils le veulent. Marchez à travers les rues typiques et laissez-vous charmer.</p>
+                    <ul>
+                        {/* <ReactTooltip id="google-maps-tooltip" type="light" className="disabled-tooltip" place="bottom">
+                            <p>{this.props.en == true ? 'Click here to open in Google Maps' : 'Cliquer pour ouvrir dans Google Maps'}</p>
+                        </ReactTooltip> */}
+                        <li><i class="fas fa-walking fa-fw"></i><a href="https://www.walkscore.com/score/4217-rue-fabre-montr%C3%A9al-qc-canada" target="_blank" data-tip data-for="bike-walk-score-tooltip">Walkscore: 94</a></li>
+                        <li><i class="fas fa-bicycle fa-fw"></i><a href="https://www.walkscore.com/score/4217-rue-fabre-montr%C3%A9al-qc-canada" target="_blank" data-tip data-for="bike-walk-score-tooltip">Bikescore: 100</a></li>
+                        {/* <ReactTooltip id="bike-walk-score-tooltip" type="light" className="disabled-tooltip" place="bottom">
+                            <p>{this.props.en == true ? 'Click here to learn more about Walkscore' : 'Cliquez ici pour en apprendre plus sur Walkscore.'}</p>
+                        </ReactTooltip> */}
+                    </ul>
                 </div>
         
                 <div class="stm-text-container">
                     <p>Pour ceux qui préfèrent utiliser les transports, le quartier met à disposition ses nombreuses lignes de bus, et plusieurs entrées de métro. Le Plateau Mont-Royal agit comme un carrefour central entre les nombreux accès qui l’entourent. Vous n’aurez jamais aucun mal à vous rendre à votre destination.</p>
+                    <div class="map-intro">
+                        <p>Vous pouvez voir sur la carte les nombreuses stations de metro et bus à proximité.</p>
+                        <img src="/images/right-arrow-light.png" />
+                    </div>
                 </div>
 
                 <div class="img-container">
